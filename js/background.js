80d3927d5abca1600c1836447e9a7e6495bfc81d
김종원 <tank3a@gmail.com>
2021-06-13 13:56:18 +0900

    Modified icons of navbar

--- js/background.js
+++ js/background.js
@@ -3,23 +3,27 @@ const background__location = document.querySelector(".background__location");
 
 const locate = ["Jeonju, korea",
                 "212 caves rd,siesta park wa 6280, australia, siesta park",
-                "Bockhütte, garmisch partenkirchen, germany",
+                "Bockhütte, Germany",
                 "King city, canada",
                 "Princeton, canada",
                 "Silverthorne, united states",
                 "Smalfjord, tana, norway",
-                "Stakkholtsgja canyon, iceland",
+                "Stakkholtsgjá, iceland",
                 "Tokyo, japan",
                 "Yosemite valley, united states"];
 
 const IMG_NUMBER = 10;
 
 function paintLocation(imgNumber) {
+    const a = document.createElement("a");
     const span = document.createElement("span");
     console.log(locate[imgNumber-1]);
     const locationName = locate[imgNumber-1];
+    a.href = `https://www.google.com/maps/place/${locationName}`;
+    a.target ="_blank";
+    a.appendChild(span);
     span.innerText = locationName;
-    background__location.appendChild(span);
+    background__location.appendChild(a);
 }
 
 function paintImage(imgNumber) {
--- js/navigation.js
+++ js/navigation.js
@@ -1,11 +1,17 @@
 const nav__icon = document.querySelector(".navigator__icon"),
-nav__container = document.querySelector(".nav__container");
+nav__container = document.querySelector(".nav__container"),
+bars = document.querySelector(".fa-bars"),
+times = document.querySelector(".fa-times");
 
 function navClickHandle(event) {
     if(nav__container.classList.contains("nav__container__show")) {
+        times.classList.add("image__hidden");
+        bars.classList.remove("image__hidden");
         nav__container.classList.add("nav__container__hidden");
         nav__container.classList.remove("nav__container__show");
     } else {
+        bars.classList.add("image__hidden");
+        times.classList.remove("image__hidden");
         nav__container.classList.add("nav__container__show");
         nav__container.classList.remove("nav__container__hidden");
 
